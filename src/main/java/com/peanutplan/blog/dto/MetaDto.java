9860d23ec6379b0fab80391b1514d6f15d6550a8
peanutnowing <deble_king@126.com>
2019-03-05 18:15:48 +0800

    SpringBoot + Mybatis + thymeleaf 搭建的个人博客

---
+++ src/main/java/com/peanutplan/blog/dto/MetaDto.java
@@ -0,0 +1,17 @@
+package com.peanutplan.blog.dto;
+
+
+import com.peanutplan.blog.model.vo.MetaVo;
+
+public class MetaDto extends MetaVo {
+
+    private int count;
+
+    public int getCount() {
+        return count;
+    }
+
+    public void setCount(int count) {
+        this.count = count;
+    }
+}
---
+++ src/main/java/com/peanutplan/blog/service/IOptionService.java
@@ -0,0 +1,28 @@
+package com.peanutplan.blog.service;
+
+
+import com.peanutplan.blog.model.vo.OptionVo;
+
+import java.util.List;
+import java.util.Map;
+
+/**
+ * options的接口
+ * Created by BlueT on 2017/3/7.
+ */
+public interface IOptionService {
+
+    void insertOption(OptionVo optionVo);
+
+    void insertOption(String name, String value);
+
+    List<OptionVo> getOptions();
+
+
+    /**
+     * 保存一组配置
+     *
+     * @param options
+     */
+    void saveOptions(Map<String, String> options);
+}
